d5382a85504d5d3cae55ffd91f4ddbee959f3bb0
HongjaePark <chicchocs@naver.com>
2023-06-27 18:00:54 +0900

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Widget Example Test3',
+      title: 'Widget Example Test5',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
